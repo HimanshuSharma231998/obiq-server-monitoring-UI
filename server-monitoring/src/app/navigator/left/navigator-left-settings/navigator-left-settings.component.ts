54a113b550d3cb0be2819203e5e09a33ee434c72
Himanshuopkey <139320785+Himanshuopkey@users.noreply.github.com>
2024-12-12 17:44:12 +0530

    Corrected call Update one

--- server-monitoring/src/app/navigator/left/navigator-left-settings/navigator-left-settings.component.ts
+++ server-monitoring/src/app/navigator/left/navigator-left-settings/navigator-left-settings.component.ts
@@ -80,12 +80,12 @@ export class NavigatorLeftSettingsComponent implements OnInit  {
 
   }
   renameView(view){
-    
+    debugger;
     
    
     let form_url = environment.BASE_OBIQ_SERVER_URL + "OpkeyObiqServerApi/OpkeyTraceIAAnalyticsApi/TelemetryViewController/renameView";
 
-    let form_data = { viewId: view.viewId,viewName:view.viewName,projectId:this.service_data.UserDto.ProjectDTO.P_ID };
+    let form_data = { viewId: view.viewId,viewName:view.viewName,projectId:this.service_data.UserDto.ProjectDTO.P_ID,userId:this.service_data.UserDto.UserDTO.U_ID };
 
     this.app_service.make_post_server_call(form_url, form_data)
       .subscribe({
